97b980966b72906175a3743293c30999343255bc
ondrejhonus <honusondrej@gmail.com>
2024-09-16 16:49:08 +0200

    new traintracks

---
+++ scripts/train.gd
@@ -0,0 +1,12 @@
+extends CharacterBody3D
+
+@export var speed = 10
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	direction.x += 1
+	target_velocity.x = direction.x * speed
+	velocity = target_velocity
+	move_and_slide()
