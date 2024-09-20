eae017fa871745ca7511e974bbaf621dce16b007
ondrejhonus <honusondrej@gmail.com>
2024-09-20 09:06:57 +0200

    train arrives in XX time counter

--- scenes/countdown.gd
+++ scenes/countdown.gd
@@ -2,8 +2,10 @@ extends Label3D
 
 var time_left = 10
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time_left -= delta
-	$RichTextLabel.text = "The train arrives in %d seconds" % [time_left]
-	pass
+	if time_left >= 0:
+		time_left -= delta
+		$RichTextLabel.text = " The train arrives in %d seconds" % [time_left]
+	else:
+		$RichTextLabel.text = ""
+		
